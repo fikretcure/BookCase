89fbf0173d5e992cb12a4db8243359179f29721c
fikretcure <fikretcure@gmail.com>
2023-01-26 15:20:12 +0300

    Updated php_doc

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,30 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\AuthorRepository;
+use Illuminate\Http\JsonResponse;
 
 /**
  *
  */
 class AuthorController extends Controller
 {
+
     /**
      * @var AuthorRepository
      */
-    private $authorRepository;
+    private AuthorRepository $authorRepository;
 
     /**
-     * @param AuthorRepository $authorRepository
+     * @param  AuthorRepository  $authorRepository
      */
     public function __construct(AuthorRepository $authorRepository)
     {
         $this->authorRepository = $authorRepository;
     }
 
-
     /**
      * @return \Illuminate\Http\JsonResponse
      */
-    public function get()
+    public function get(): JsonResponse
     {
         return response()->json($this->authorRepository->get());
     }
--- app/Repositories/AuthorRepository.php
+++ app/Repositories/AuthorRepository.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class AuthorRepository
 {
+
     /**
      * @var Model|Author
      */
