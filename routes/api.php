23959e6873a32f12b1162ff8b54fb0caf2cda3bf
fikretcure <fikretcure@gmail.com>
2023-01-26 13:12:39 +0300

    Added route in api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('test', function () {
-    return (new \App\Repositories\AuthorRepository())->get();
-});
+Route::get('author', [AuthorController::class, 'get']);
