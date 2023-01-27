4414c734f2005572ba569b6b55c7249926f77c8e
fikretcure <fikretcure@gmail.com>
2023-01-27 19:53:52 +0300

    Updated create in AuthorController

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,24 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthorCreateRequest;
 use App\Repositories\AuthorRepository;
+use App\Services\AuthorService;
+use App\Services\DocumentService;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Throwable;
 
+/**
+ *
+ */
 class AuthorController extends Controller
 {
+
     /**
      * @var AuthorRepository
      */
     private AuthorRepository $authorRepository;
+    /**
+     * @var AuthorService
+     */
+    private AuthorService $authorService;
 
     /**
      * @param AuthorRepository $authorRepository
+     * @param AuthorService $authorService
      */
-    public function __construct(AuthorRepository $authorRepository)
+    public function __construct(AuthorRepository $authorRepository, AuthorService $authorService)
     {
         $this->authorRepository = $authorRepository;
+        $this->authorService = $authorService;
     }
 
     /**
@@ -35,9 +48,13 @@ class AuthorController extends Controller
     /**
      * @param AuthorCreateRequest $request
      * @return Model|Builder
+     * @throws Throwable
      */
     public function create(AuthorCreateRequest $request): Model|Builder
     {
-        return $this->authorRepository->create($request->validated());
+        (new DocumentService())->hasDocument($request->validated("avatar"));
+        $author = $this->authorRepository->create($request->validated());
+        $this->authorService->generateAvatarFromId($author->id, 123);
+        return $author;
     }
 }
