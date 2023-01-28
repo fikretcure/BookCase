562cd9151e891d047e41093da8c8bab16884c2d8
fikretcure <fikretcure@gmail.com>
2023-01-28 18:13:01 +0300

    Added avatar in Author

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ *
+ */
 class Author extends Model
 {
     use HasFactory, SoftDeletes;
@@ -26,16 +30,27 @@ class Author extends Model
     /**
      * @return string
      */
-    public function getFullNameAttribute()
+    public function getFullNameAttribute(): string
     {
-        return $this->attributes['name'].' '.$this->attributes['surname'];
+        return $this->attributes['name'] . ' ' . $this->attributes['surname'];
     }
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @return HasMany
      */
     public function book(): HasMany
     {
         return $this->hasMany(Book::class);
+    }
+
+
+    /**
+     * @return Attribute
+     */
+    protected function avatar(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value ? env("APP_URL") . "/storage/avatar/" . $value : null,
+        );
     }
 }
