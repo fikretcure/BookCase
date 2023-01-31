01bed3e51969acb95c47035bb6bfe7ddf31176a5
fikretcure <fikretcure@gmail.com>
2023-01-31 13:24:27 +0300

    Updated document in Author

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -47,20 +47,16 @@ class Author extends Model
         return $this->hasMany(Book::class);
     }
 
-
-    /**
-     * @return Attribute
-     */
-    protected function avatar(): Attribute
-    {
-        return Attribute::make(
-            get: fn($value) => $value ? env("APP_URL") . "/storage/avatar/" . $value : null,
-        );
-    }
-
-
     /**
      * @var string
      */
     public static string $reg_code = "Y";
+
+    /**
+     * @return MorphOne
+     */
+    public function document(): MorphOne
+    {
+        return $this->morphOne(Document::class, 'model');
+    }
 }
