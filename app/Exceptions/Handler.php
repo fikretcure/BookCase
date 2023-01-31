a88ba17bdd7d4fb5986873623a3f6cf53adaf4a4
fikretcure <fikretcure@gmail.com>
2023-01-31 16:38:52 +0300

    Updated register in Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,17 @@
 
 namespace App\Exceptions;
 
+use App\Traits\Responsed;
+use Illuminate\Database\RecordsNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
+    use Responsed;
+
     /**
      * A list of exception types with their corresponding custom log levels.
      *
@@ -43,6 +49,19 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
+        $this->renderable(function (Throwable $exception, $request) {
+            if ($exception instanceof ValidationException) {
+                return $this->fail($exception->validator->getMessageBag())->send(402);
+            }
+
+            if ($exception->getPrevious() instanceof RecordsNotFoundException) {
+                return $this->fail()->mes('İşlemini yapmak istediğiniz kayıt malesef bulunamadı.Yolunda gitmeyen bir şeyler var !')->send();
+            }
+
+            return $this->fail($exception->getMessage())->mes("'Dostumm ... Bir şeyler ters gitti anlıyo musun !... tekrar denemelisin !'")->send(402);
+        });
+
+
         $this->reportable(function (Throwable $e) {
             //
         });
